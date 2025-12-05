3635425ea768893659ef3906fa394b3c5403120f
Jason Louie <jason.louie29@myhunter.cuny.edu>
2025-12-05 16:09:52 -0500

    Init repo. File organization and folder creation.

---
+++ src/components/Button.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import { MouseEventHandler } from "react";
+
+interface ButtonProps {
+    id?: string;
+    className: string;
+    href?: string;
+    onClick?: MouseEventHandler;
+    children: React.ReactNode;
+    buttonType?: "button" | "submit" | "reset";
+    download?: string;
+    disabled?: boolean;
+    hoverEffect?: string;
+}
+
+export default function Button({
+    id,
+    className,
+    href,
+    onClick,
+    children,
+    buttonType,
+    download,
+    disabled,
+    hoverEffect
+} : ButtonProps) {
+    const classes = ``;
+
+    const renderButton = () => {
+        return (
+            <button id={id} className={classes}>
+                {children}
+            </button>
+        );
+    };
+
+    const renderLink = () => {
+        return (
+            <Link href={href!} className={classes}>
+                {children}
+            </Link>
+        )
+    }
+
+    const renderDownload = () => {
+        return (
+            <a href={href!} className={classes} >{children}</a>
+        );
+    }
+
+    return href ? (download ? renderDownload() : renderLink()) : renderButton();
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import Button from "./Button";
+
+export default function Navbar() {
+    const navElementClasses = ``;
+    return (
+        <nav>
+            <Button href="/" id="home" className={navElementClasses}>Home</Button>
+        </nav>
+    );
+}
